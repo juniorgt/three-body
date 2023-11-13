f273d864273ff82517e3bf8b08e311ad4f7b4b88
juniorgtt <junior.guevarat@gmail.com>
2023-11-12 23:58:13 -0500

    add package ODESolvers

---
+++ code/ODESolvers/methods.py
@@ -0,0 +1,199 @@
+import numpy as np
+
+from .coefficients import *
+
+rk_methods = {
+    "euler": np.array([[0, 0], [None, a5]]),
+    "midpoint": np.array([[0, 0, 0], [a6, a6, 0], [None, 0, a5]]),
+    "heun": np.array([[0, 0, 0], [a5, a5, 0], [None, a6, a6]]),
+    "ralston": np.array([[0, 0, 0], [a10, a10, 0], [None, a2, a7]]),
+    "kutta_second": np.array([[0, 0, 0], [a6, a6, 0], [None, 0, a5]]),
+    "kutta_third": np.array(
+        [[0, 0, 0, 0], [a6, a6, 0, 0], [a5, -a5, a11, 0], [None, a12, a10, a12]]
+    ),
+    "heun_third": np.array(
+        [[0, 0, 0, 0], [a9, a9, 0, 0], [a10, 0, a10, 0], [None, a2, 0, a7]]
+    ),
+    "ralston_third": np.array(
+        [[0, 0, 0, 0], [a6, a6, 0, 0], [a7, 0, a7, 0], [None, a13, a9, a14]]
+    ),
+    "ssprk3": np.array(
+        [[0, 0, 0, 0], [a5, a5, 0, 0], [a6, a2, a2, 0], [None, a12, a12, a10]]
+    ),
+    "original_rk": np.array(
+        [
+            [0, 0, 0, 0, 0],
+            [a6, a6, 0, 0, 0],
+            [a6, 0, a6, 0, 0],
+            [a5, 0, 0, a5, 0],
+            [None, a12, a9, a9, a12],
+        ]
+    ),
+    "third_eight": np.array(
+        [
+            [0, 0, 0, 0, 0],
+            [a9, a9, 0, 0, 0],
+            [a10, -a9, a5, 0, 0],
+            [a5, a5, -a5, a5, 0],
+            [None, a8, a3, a3, a8],
+        ]
+    ),
+    "gill": np.array(
+        [
+            [0, 0, 0, 0, 0],
+            [a6, a6, 0, 0, 0],
+            [a6, a6 * (-a5 + a15), a5 - a6 * a15, 0, 0],
+            [a5, 0, -a6 * a15, a5 + a6 * a15, 0],
+            [None, a12, (a11 - a15) * a12, (a11 + a15) * a12, a12],
+        ]
+    ),
+    # Adaptive step RK methods
+    "heun_euler": np.array([[0, 0, 0], [a5, a5, 0], [None, a6, a6], [None, a5, 0]]),
+    "fehlberg_rk12": np.array(
+        [
+            [0, 0, 0, 0],
+            [a6, a6, 0, 0],
+            [a5, a16, a17, 0],
+            [None, a16, a17, 0],
+            [None, a18, a17, a18],
+        ]
+    ),
+    "bogacki_shampine": np.array(
+        [
+            [0, 0, 0, 0, 0],
+            [a6, a6, 0, 0, 0],
+            [a7, 0, a7, 0, 0],
+            [a5, a13, a9, a14, 0],
+            [None, a13, a9, a14, 0],
+            [None, a19, a2, a9, a8],
+        ]
+    ),
+    "fehlberg": np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [a2, a2, 0, 0, 0, 0, 0],
+            [a3, b31, b32, 0, 0, 0, 0],
+            [a4, b41, b42, b43, 0, 0, 0],
+            [a5, b51, b52, b53, b54, 0, 0],
+            [a6, b61, b62, b63, b64, b65, 0],
+            [None, p1, 0, p3, p4, p5, r6],
+            [None, c1, 0, c3, c4, c5, 0],
+        ]
+    ),
+    "cash_karp": np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [a1, a1, 0, 0, 0, 0, 0],
+            [ck1, ck2, ck3, 0, 0, 0, 0],
+            [ck4, ck1, ck5, ck6, 0, 0, 0],
+            [a5, ck7, ck8, ck9, ck10, 0, 0],
+            [ck11, ck12, ck13, ck14, ck15, ck16, 0],
+            [None, ck17, 0, ck18, ck19, 0, ck20],
+            [None, ck21, 0, ck22, ck23, ck24, a2],
+        ]
+    ),
+    "dormand_prince": np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [a1, a1, 0, 0, 0, 0, 0, 0],
+            [ck1, ck2, ck3, 0, 0, 0, 0, 0],
+            [dp1, dp2, dp3, dp4, 0, 0, 0, 0],
+            [dp5, dp6, dp7, dp8, dp9, 0, 0, 0],
+            [a5, dp10, dp11, dp12, dp13, dp14, 0, 0],
+            [a5, dp15, 0, dp16, dp17, dp18, dp19, 0],
+            [None, dp15, 0, dp16, dp17, dp18, dp19, 0],
+            [None, dp20, 0, dp21, dp22, dp23, dp24, a20],
+        ]
+    ),
+}
+
+# PECE ------------------------------------------------------------------------------------
+# Predictors: (Adams-Bashforth)
+predictor = [
+    np.array([1]),
+    np.array([3.0 / 2.0, -1.0 / 2.0]),
+    np.array([23.0 / 12.0, -16.0 / 12.0, 5.0 / 12.0]),
+    np.array([55.0 / 24.0, -59.0 / 24.0, 37.0 / 24.0, -9.0 / 24.0]),
+    np.array(
+        [
+            1901.0 / 720.0,
+            -2774.0 / 720.0,
+            2616.0 / 720.0,
+            -1274.0 / 720.0,
+            251.0 / 720.0,
+        ]
+    ),
+]
+
+# Correctors: (Adams-Moulton)
+corrector = [
+    np.array([0.5, 0.5]),
+    np.array([5.0 / 12.0, 2.0 / 3.0, -1.0 / 12.0]),
+    np.array([9.0 / 24.0, 19.0 / 24.0, -5.0 / 24.0, 1.0 / 24.0]),
+    np.array(
+        [251.0 / 720.0, 646.0 / 720.0, -264.0 / 720.0, 106.0 / 720.0, -19.0 / 720.0]
+    ),
+]
+
+# SymIntegrator ---------------------------------------------------------------------------
+# Simplectic integrators
+sym_methods = {
+    "euler": np.array([[1], [1]]),
+    "verlet": np.array([[0, 1], [0.5, 0.5]]),
+    "ruth": np.array(
+        [[1, -2.0 / 3.0, 2.0 / 3.0], [-1.0 / 24.0, 3.0 / 4.0, 7.0 / 24.0]]
+    ),
+    # leapfrog_yoshida
+    "forest_ruth": np.array(
+        [
+            [
+                (1.0 / (2.0 * (2.0 - sym_crt))),
+                ((1.0 - sym_crt) / (2.0 * (2.0 - sym_crt))),
+                ((1.0 - sym_crt) / (2.0 * (2.0 - sym_crt))),
+                (1.0 / (2.0 * (2.0 - sym_crt))),
+            ],
+            [
+                (1.0 / (2.0 - sym_crt)),
+                (-sym_crt / (2.0 - sym_crt)),
+                (1.0 / (2.0 - sym_crt)),
+                0.0,
+            ],
+        ]
+    ),
+    "VEFRL": np.array(
+        [
+            [
+                0.0,
+                (1.0 - 2.0 * sym_lam1) / 2.0,
+                sym_lam1,
+                sym_lam1,
+                (1.0 - 2.0 * sym_lam1) / 2.0,
+            ],
+            [
+                sym_eps1,
+                sym_ksi1,
+                (1.0 - 2.0 * (sym_ksi1 + sym_eps1)),
+                sym_ksi1,
+                sym_eps1,
+            ],
+        ]
+    ),
+    "PEFRL": np.array(
+        [
+            [
+                sym_eps2,
+                sym_ksi2,
+                (1.0 - 2.0 * (sym_ksi2 + sym_eps2)),
+                sym_ksi2,
+                sym_eps2,
+            ],
+            [
+                (1.0 - 2.0 * sym_lam2) / 2.0,
+                sym_lam2,
+                sym_lam2,
+                (1.0 - 2.0 * sym_lam2) / 2.0,
+                0.0,
+            ],
+        ]
+    ),
+}
---
+++ code/ODESolvers/rungekutta.py
@@ -0,0 +1,110 @@
+import numpy as np
+
+
+class RKMethod:
+    def __init__(
+        self,
+        method,
+        function,
+        t0=0.0,
+        t_bound=1.0,
+        y0=[0.0],
+        first_step=1.0,
+        adaptive=False,
+        tolerance=1.0e-20,
+        endpoint=True,
+    ):
+        self.method = method
+        self.function = function
+        self.t0 = t0
+        self.t_bound = t_bound
+        self.y0 = y0
+        self.first_step = first_step
+        self.adaptive = adaptive
+        self.tolerance = tolerance
+        self.endpoint = endpoint
+
+    def _step_impl(self, t, y, h, calc_error):
+        """Perform a single Runge-Kutta step.
+
+        Parameters
+        ----------
+        t : float
+            Current time.
+        y : ndarray, shape (n,)
+            Current state.
+        h : float
+            Step to use.
+        """
+
+        k = np.zeros((len(y), self.method.shape[1] - 1))
+        for i in range(self.method.shape[1] - 1):
+            k[:, i] = self.function(
+                t + h * self.method[i][0], y + h * k.dot(self.method[i, 1:])
+            )
+
+        y_new1 = y + h * k.dot(self.method[-1, 1:])
+        if calc_error:
+            y_new2 = y + h * k.dot(self.method[-2, 1:])
+            error = np.absolute(y_new1 - y_new2)
+            return t + h, y_new2, error
+        else:
+            return t + h, y_new1
+
+    def run(self):
+        y0 = np.asarray(self.y0)
+        y0 = y0.astype(float, copy=False)
+        if y0.ndim != 1:
+            raise ValueError("'y0' must be 1-dimensional.")
+
+        calc_error = True if self.method.shape[0] == self.method.shape[1] + 1 else False
+
+        stepsize = np.absolute(self.first_step)
+
+        t = [self.t0]
+        y = [y0]
+        if calc_error:
+            error = [np.zeros(y0.shape[0])]
+
+        while t[-1] < self.t_bound:
+            if self.adaptive:
+                stepsize = (
+                    stepsize
+                    * 0.9
+                    * np.minimum(
+                        np.maximum(
+                            np.sqrt(
+                                0.5
+                                * np.amin(np.divide(self.tolerance, np.amax(error[-1])))
+                            ),
+                            0.3,
+                        ),
+                        2.0,
+                    )
+                )
+            if self.endpoint:
+                if t[-1] + stepsize > self.t_bound:
+                    stepsize = np.absolute(self.t_bound - t[-1])
+
+            h = stepsize
+            new_values = self._step_impl(t[-1], y[-1], h, calc_error)
+            t.append(new_values[0])
+            y.append(new_values[1])
+            if calc_error:
+                error.append(new_values[2])
+
+        y = np.array(y)
+        return t, y
+
+
+if __name__ == "__main__":
+
+    def fun(t, y):
+        return y - t**2 + 1
+
+    y0 = 0.5
+
+    rk = RKMethod(np.array([[0, 0], [None, 1.00000]]), fun, 0, 2, [y0], 0.2)
+    a, b = rk.run()
+    for x, y in zip(a, b):
+        print(x, y)
