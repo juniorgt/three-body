6e792ce2158667d2d13c04117538d707784e397c
juniorgtt <junior.guevarat@gmail.com>
2023-11-30 23:10:24 -0500

    Refactor simulation_butterfly_I function to use different ODE solvers and plot the results

--- code/butterfly_I.py
+++ code/butterfly_I.py
@@ -1,51 +1,31 @@
 from BodySytem import BodySystem
+from ODESolvers.methods import rk_methods, sym_methods
 
 
-def simulation_butterfly_I():
-    butterflyI_init_setup = {
-        "name": "Butterfly_I",
+def simulation_butterfly_I(h):
+    init_setup = {
+        "name": f"Butterfly_I_{str(h)}",
         "G": 1,
         "M": [1, 1, 1],
-        "x": [-1.0, 1.0, 0.0],
-        "y": [0.0, 0.0, 0.0],
-        "vx": [0.306892758965492, 0.306892758965492, -2 * 0.306892758965492],
-        "vy": [0.125506782829762, 0.125506782829762, -2 * 0.125506782829762],
-        "T": 6.23564136316479 * 1,
-        "steps": 100000 * 1,
+        "y1": [-1.0, 0.306892758965492, 0.0, 0.125506782829762],
+        "y2": [1.0, 0.306892758965492, 0.0, 0.125506782829762],
+        "y3": [0.0, -2 * 0.306892758965492, 0.0, -2 * 0.125506782829762],
+        "T": 6.23564136316479,
+        "h": h,
     }
 
-    butterflyI_euler = BodySystem(butterflyI_init_setup, ODESolver="Euler")
-    butterflyI_euler.run_simulation()
-    butterflyI_euler.plot_orbit()
-    butterflyI_euler.plot_total_energy()
-    butterflyI_euler.plot_angular_momentum()
-    butterflyI_euler.plot_momentum_lineal()
-    butterflyI_euler.save_error()
+    for rk in rk_methods.keys():
+        BS = BodySystem(init_setup, ODESolver="rk", method=rk)
+        BS.run_simulation()
+        BS.plot()
 
-    butterflyI_rk4 = BodySystem(butterflyI_init_setup, ODESolver="RK4")
-    butterflyI_rk4.run_simulation()
-    butterflyI_rk4.plot_orbit()
-    butterflyI_rk4.plot_total_energy()
-    butterflyI_rk4.plot_angular_momentum()
-    butterflyI_rk4.plot_momentum_lineal()
-    butterflyI_rk4.save_error()
-
-    butterflyI_rkf = BodySystem(butterflyI_init_setup, ODESolver="RKF")
-    butterflyI_rkf.run_simulation()
-    butterflyI_rkf.plot_orbit()
-    butterflyI_rkf.plot_total_energy()
-    butterflyI_rkf.plot_angular_momentum()
-    butterflyI_rkf.plot_momentum_lineal()
-    butterflyI_rkf.save_error()
-
-    butterflyI_verlet = BodySystem(butterflyI_init_setup, ODESolver="Verlet")
-    butterflyI_verlet.run_simulation()
-    butterflyI_verlet.plot_orbit()
-    butterflyI_verlet.plot_total_energy()
-    butterflyI_verlet.plot_angular_momentum()
-    butterflyI_verlet.plot_momentum_lineal()
-    butterflyI_verlet.save_error()
+    for sym in sym_methods.keys():
+        BS = BodySystem(init_setup, ODESolver="sym", method=sym)
+        BS.run_simulation()
+        BS.plot()
 
 
 if "__main__" == __name__:
-    simulation_butterfly_I()
+    list_first_steps = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
+    for h in list_first_steps:
+        simulation_butterfly_I(h)
