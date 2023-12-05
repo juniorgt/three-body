244d77fab4273a5f518ee0e0b2be7eb1cab5af4f
Junior Guevara Tanta <80008687+juniorgt@users.noreply.github.com>
2023-12-05 17:41:46 +0000

    Refactor simulation_butterfly_I function and remove unused code

--- code/butterfly_I.py
+++ code/butterfly_I.py
@@ -2,9 +2,9 @@ from bodySytem import BodySystem
 from ODESolvers.methods import rk_methods, sym_methods
 
 
-def simulation_butterfly_I(h, is_save=True):
+def simulation_butterfly_I(h):
     init_setup = {
-        "name": f"Butterfly_I_{str(h)}",
+        "name": "Butterfly-I",
         "G": 1,
         "M": [1, 1, 1],
         "y1": [-1.0, 0.306892758965492, 0.0, 0.125506782829762],
@@ -15,16 +15,12 @@ def simulation_butterfly_I(h, is_save=True):
     }
 
     for rk in rk_methods.keys():
-        BS = BodySystem(
-            init_setup, ODESolver="rk", method=rk, is_save_simulation_data=is_save
-        )
+        BS = BodySystem(init_setup, ODESolver="rk", method=rk)
         BS.run_simulation()
         BS.plot()
 
     for sym in sym_methods.keys():
-        BS = BodySystem(
-            init_setup, ODESolver="sym", method=sym, is_save_simulation_data=is_save
-        )
+        BS = BodySystem(init_setup, ODESolver="sym", method=sym)
         BS.run_simulation()
         BS.plot()
 
@@ -33,5 +29,3 @@ if "__main__" == __name__:
     list_first_steps = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
     for h in list_first_steps:
         simulation_butterfly_I(h)
-    for h in [1e-6, 1e-7]:
-        simulation_butterfly_I(h, is_save=False)
